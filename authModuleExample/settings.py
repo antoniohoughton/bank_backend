8d6687dcc5f4601b8377daabbc56bc8f24c398b8
casierrav <jerarquia04@gmail.com>
2021-09-30 09:19:37 -0500

    Changes to make easy setup and installation

--- authModuleExample/settings.py
+++ authModuleExample/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
+from pathlib  import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,6 +42,17 @@ INSTALLED_APPS = [
     'authAppExample',
 ]
 
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME'   : timedelta(minutes=10),
+    'REFRESH_TOKEN_LIFETIME'  : timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS'   : False,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'UPDATE_LAST_LOGIN'       : False,
+    'ALGORITHM'               : 'HS256',
+    'USER_ID_FIELD'           : 'id',
+    'USER_ID_CLAIM'           : 'user_id',
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -50,6 +62,15 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permission.AllowAny',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 ROOT_URLCONF = 'authModuleExample.urls'
 
@@ -77,8 +98,12 @@ WSGI_APPLICATION = 'authModuleExample.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE'   : 'django.db.backends.postgresql_psycopg2',
+        'NAME'     : 'p8_bank',
+        'USER'     : 'postgres',
+        'PASSWORD' : 'postgres',
+        'HOST'     : 'localhost',
+        'PORT'     : '5432'
     }
 }
 
