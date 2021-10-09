ba26c7bffac599f9708bded860ab06b5c3d01029
casierrav <jerarquia04@gmail.com>
2021-10-09 15:02:00 -0500

    Add new endpoints for more services deployment

--- authProjectExample/urls.py
+++ authProjectExample/urls.py
@@ -9,9 +9,12 @@ urlpatterns = [
     path('refresh/',                                TokenRefreshView.as_view()), # generate new access token
     path('user/',                                   authAppViews.UserCreateView.as_view()), # create a new user
     path('user/<int:pk>/',                          authAppViews.UserDetailView.as_view()), # check info for an specific user based on id(pk)
-    path('transaction/',                            authAppViews.TransactionCreateView.as_view()), # create a new transaction
+    path('transaction/create/',                     authAppViews.TransactionCreateView.as_view()), # create a new transaction
     path('transaction/<int:user>/<int:pk>/',        authAppViews.TransactionsDetailView.as_view()), # view information for a transaction
-    path('transactions/<int:user>/<int:account>/',  authAppViews.TransactionsAccountView.as_view()), # view all transactions for an specific account
     path('transaction/update/<int:user>/<int:pk>/', authAppViews.TransactionsUpdateView.as_view()), # update a transaction
     path('transaction/remove/<int:user>/<int:pk>/', authAppViews.TransactionsDeleteView.as_view()), # delete a transaction
+    path('transactions/<int:user>/<int:account>/',  authAppViews.TransactionsAccountView.as_view()), # view all transactions for an specific account
+    path('deposit/create/',                     authAppViews.DepositCreateView.as_view()), # create a new deposit
+    path('deposit/<int:user>/<int:pk>/',        authAppViews.DepositDetailView.as_view()), # view information for a deposit
+    path('deposits/<int:user>/<int:account>/',  authAppViews.DepositsAccountView.as_view()), # view all depositss for an specific account
 ]
